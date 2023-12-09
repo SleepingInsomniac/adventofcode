e3252c2d53a61b29e09d5cbf73792b2ac703c103
Alex Clink <alexclink@gmail.com>
2023-12-09 01:05:44 -0500

    Day 9, Part 1

---
+++ 2023-12-09/part_1.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+class Sequence
+  attr_reader :numbers
+
+  def initialize(numbers) = @numbers = numbers
+  def extrapolate = Sequence.new(@numbers.each_cons(2).map { |a, b| b - a })
+  def zeros? = @numbers.all?(&:zero?)
+  def predict = zeros? ? 0 : @numbers.last + extrapolate.predict
+end
+
+sequences = File.readlines(File.join(__dir__, 'input.txt')).map do |l|
+  Sequence.new(l.split.map(&:to_i))
+end
+
+puts sequences.map(&:predict).reduce(&:+)
