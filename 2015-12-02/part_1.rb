6a42e04f4f977292c34434c249ecd7725baa05c0
Alex Clink <alexclink@gmail.com>
2025-11-18 15:45:04 -0500

    2015 day 2, part 1 and 2

--- 2015-12-02/part_1.rb
+++ 2015-12-02/part_1.rb
@@ -1,14 +1,21 @@
 #!/usr/bin/env ruby
 
-# <%= puzzle_url %>
+# https://adventofcode.com/2015/day/2
 
 def solve(input)
-  # ...
+  l, w, h = input.split("x").map(&:to_i)
+  side_a = l * w
+  side_b = w * h
+  side_c = h * l
+
+  2 * side_a + 2 * side_b + 2 * side_c + [side_a, side_b, side_c].min
 end
 
 # Tests ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 tests = {
+  "2x3x4" => 58,
+  "1x1x10" => 43,
 }.map do |(input, expectation)|
   result = solve(input)
   [input, [result == expectation, "Expected #{result} to equal #{expectation}"]]
@@ -18,7 +25,13 @@ end.to_h
 
 if tests.values.all? { |v| v[0] }
   File.open(File.join(__dir__, 'input.txt'), 'r') do |file|
-    # line = file.readline.chomp
+    total = 0
+    while line = file.readline.chomp
+      total += solve(line)
+    end
+  rescue EOFError
+  ensure
+    puts total
   end
 else
   $stderr.puts tests
--- 2015-12-02/part_2.rb
+++ 2015-12-02/part_2.rb
@@ -1,14 +1,21 @@
 #!/usr/bin/env ruby
 
-# <%= puzzle_url %>
+# https://adventofcode.com/2015/day/2#part2
 
 def solve(input)
-  # ...
+  l, w, h = input.split("x").map(&:to_i)
+
+  d1, d2 = [l, w, h].sort[0..1]
+  bow = l * w * h
+
+  2 * d1 + 2 * d2 + bow
 end
 
 # Tests ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 tests = {
+  "2x3x4" => 34,
+  "1x1x10" => 14,
 }.map do |(input, expectation)|
   result = solve(input)
   [input, [result == expectation, "Expected #{result} to equal #{expectation}"]]
@@ -18,7 +25,13 @@ end.to_h
 
 if tests.values.all? { |v| v[0] }
   File.open(File.join(__dir__, 'input.txt'), 'r') do |file|
-    # line = file.readline.chomp
+    total = 0
+    while line = file.readline.chomp
+      total += solve(line)
+    end
+  rescue EOFError
+  ensure
+    puts total
   end
 else
   $stderr.puts tests
