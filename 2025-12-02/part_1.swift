8cb7178d0cf97f3a32ff793279ee6b3ea2b71c10
Alex Clink <alexclink@gmail.com>
2025-12-02 13:04:25 -0500

    2025, day 2, part 1 : swift

---
+++ 2025-12-02/part_1.swift
@@ -0,0 +1,43 @@
+// https://adventofcode.com/2025/day/2
+
+import Foundation
+
+enum Failure: Error {
+    case message(String)
+}
+
+func solve(url: URL) async throws -> Int {
+    let handle = try FileHandle(forReadingFrom: url)
+    defer { try? handle.close() }
+
+    var buffer: [Character] = []
+    var solution : Int = 0
+    let regex = try! Regex(#"^(\d+)\1$"#)
+
+    for try await char in handle.bytes.characters {
+        if char != "," {
+            buffer.append(char)
+        } else {
+            let string = String(buffer)
+            buffer.removeAll(keepingCapacity: true)
+            let parts = string.split(separator: "-")
+            let start = Int(parts[0])!
+            let stop = Int(parts[1])!
+
+            for n in start...stop {
+                if String(n).firstMatch(of: regex) != nil {
+                    solution += n
+                }
+            }
+        }
+    }
+
+    return solution
+}
+
+let file = URL(fileURLWithPath: #filePath)
+let dir  = file.deletingLastPathComponent()
+let target = dir.appendingPathComponent("input.txt")
+
+let result = try await solve(url: target)
+print(result)
