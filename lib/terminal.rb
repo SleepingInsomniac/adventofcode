15ded1b8587c8ab3d4e5c13bd2a1d21336181904
Alex Clink <alexclink@gmail.com>
2025-12-05 14:14:41 -0500

    Update terminal class

--- lib/terminal.rb
+++ lib/terminal.rb
@@ -1,15 +1,54 @@
+require "io/console"
 require_relative "./flags"
 
 class Terminal
-  TIOCGWINSZ = 0x40087468
+  TIOCGWINSZ = 0x40087468 # Magic value for IOCTL
+  ESC = "\033"
+  CSI = "#{ESC}["
 
-  module CursorShape
-    BLINKING_BLOCK     = 1
-    STEADY_BLOCK       = 2
-    BLINKING_UNDERLINE = 3
-    STEADY_UNDERLINE   = 4
-    BLINKING_BAR       = 5
-    STEADY_BAR         = 6
+  module ANSI
+
+    module CLEAR
+      TO_END   = "0J"
+      TO_START = "1J"
+      ALL      = "2J" # Clear screen
+      SCROLL   = "3J" # Clears the scrollback buffer too
+    end
+
+    module CURSOR
+      TRUNC   = "K" # Truncate line
+      HOME    = "H" # Return to top left
+      UP      = "A"
+      DOWN    = "B"
+      FORWARD = "C"
+      BACK    = "D"
+      SAVE    = "s" # Save cursor position
+      RESTORE = "u" # Restore cursor position
+      HIDE    = "?25l"
+      SHOW    = "?25h"
+
+      module SHAPE
+        module BLOCK
+          BLINKING = 1
+          STEADY   = 2
+        end
+
+        module UNDERLINE
+          BLINKING = 3
+          STEADY   = 4
+        end
+
+        module BAR
+          BLINKING = 5
+          STEADY   = 6
+        end
+      end
+    end
+
+    module ALT_BUFFER
+      ENTER = "?1049h"
+      LEAVE = "?1049l"
+    end
   end
 
   TEXT_MODES = %i[
@@ -23,61 +62,36 @@ class Terminal
     strikethrough
   ]
 
-  def self.size
-    buf = [0, 0, 0, 0].pack("SSSS")
-    rc = io.ioctl(TIOCGWINSZ, buf)
-    raise "ioctl failed" if rc < 0
-    rows, cols, xpx, ypx = buf.unpack("SSSS")
-    { rows: rows, cols: cols, x_pixels: xpx, y_pixels: ypx }
-  end
+  def self.size = IO.console.winsize
+
+  attr_reader :x, :y, :cursor_shape, :text_mode, :fore, :back, :width, :height
 
   def initialize(io = $stdout)
     @io = io
     @x, @y = 0, 0
-    @text_mode = Flags.new(%i[])
+    @cursor_shape = ANSI::CURSOR::SHAPE::BLOCK::BLINKING
+    @text_mode = Flags.new(TEXT_MODES)
+    @height, @width = self.class.size
+    trap("SIGWINCH") do
+      @height, @width = self.class.size
+    end
   end
 
   # Move cursor to line, column
   def move(x, y)
     return if @x == x && @y == y
     @x, @y = x, y
-    @io.print "\033[#{@y + 1};#{@x + 1}H"
-    self
+    put("#{@y + 1};#{@x + 1}H")
   end
 
-  # Delete the rest of the line from cursor pos
-  def trunc
-    @io.print "\033[K"
-    self
-  end
-
-  # Clear the screen
-  def clear
-    @io.print "\033[2J"
-    self
-  end
-
-  def home
-    @x, @y = 0, 0
-    @io.print "\033[H"
-    self
-  end
-
-  # Remove the scrollback buffer
-  def clear_scroll
-    @io.print "\033[3J"
-    self
-  end
-
-  def alt_buffer
-    @io.print "\033[?1049h"
-    self
-  end
-
-  def restore_main_buffer
-    @io.print "\033[?1049l"
-    self
-  end
+  def trunc               = put(ANSI::CURSOR::TRUNK) # Delete the rest of the line from cursor pos
+  def clear               = put(ANSI::CLEAR::ALL)    # Clear the screen
+  def home                = put(ANSI::CURSOR::HOME)  # Return to top left)
+  def clear_scroll        = put(ANSI::CLEAR::SCROLL) # Remove the scrollback buffer
+  def alt_buffer          = put(ANSI::ALT_BUFFER::ENTER)
+  def restore_main_buffer = put(ANSI::ALT_BUFFER::LEAVE)
+  def show_cursor         = put(ANSI::CURSOR::SHOW)
+  def hide_cursor         = put(ANSI::CURSOR::HIDE)
 
   # Clears the terminal, allows drawing, etc. then returns to previous state
   def in_alt_buffer
@@ -87,18 +101,28 @@ class Terminal
     restore_main_buffer
   end
 
+  def steady
+    return if steady?
+    @cursor_shape += 1
+    self
+  end
+
+  def steady? = @cursor_shape.even?
+
+  def blink
+    return if blinking?
+    @cursor_shape -= 1
+    self
+  end
+
+  def blinking? = @cursor_shape.odd?
+  def block     = put(blinking? ? ANSI::CURSOR::SHAPE::BLOCK::BLINKING : ANSI::CURSOR::SHAPE::BLOCK::STEADY)
+  def underline = put(blinking? ? ANSI::CURSOR::SHAPE::UNDERLINE::BLINKING : ANSI::CURSOR::SHAPE::UNDERLINE::STEADY)
+  def bar       = put(blinking? ? ANSI::CURSOR::SHAPE::BAR::BLINKING : ANSI::CURSOR::SHAPE::BAR::STEADY)
+
   def cursor_shape=(shape_value)
-    @io.print "\033[#{shape_value}q"
-  end
-
-  def show_cursor
-    @io.print "\033[?25h"
-    self
-  end
-
-  def hide_cursor
-    @io.print "\033[?25l"
-    self
+    @cursor_shape = shape_value
+    put("#{shape_value}q")
   end
 
   def hidden_cursor
@@ -108,26 +132,19 @@ class Terminal
     show_cursor
   end
 
-  def text_mode
-    @text_mode
-  end
-
   def text_mode=(mode)
     return if mode == @text_mode
-
-    @io.print "\033[0m"
-    @io.print "\033[1m" if mode.set?(:bold)
-    @io.print "\033[2m" if mode.set?(:dim)
-    @io.print "\033[3m" if mode.set?(:italic)
-    @io.print "\033[4m" if mode.set?(:underline)
-    @io.print "\033[5m" if mode.set?(:blink)
-    @io.print "\033[7m" if mode.set?(:reverse)
-    @io.print "\033[8m" if mode.set?(:hidden)
-    @io.print "\033[9m" if mode.set?(:strikethrough)
     @text_mode = mode
+
+    put "0m"
+    TEXT_MODES.each.with_index do |m, i|
+      put "#{i}m" if mode.set?(m)
+    end
+
+    self
   end
 
-  def in(fore, back)
+  def with_color(fore, back)
     old_fore = @fore
     old_back = @back
     self.fore = fore
@@ -142,10 +159,11 @@ class Terminal
     return if @fore == color
 
     if color.nil?
-      @io.print "\033[39m"
+      put "39m"
     else
-      @io.print "\033[38;2;", color[0].to_s, ";", color[1].to_s, ";", color[2].to_s, "m"
+      put "38;2;#{color.join(";")}m"
     end
+
     @fore = color
   end
 
@@ -153,19 +171,28 @@ class Terminal
     return if @back == color
 
     if color.nil?
-      @io.print "\033[49m"
+      put "49m"
     else
-      @io.print "\033[48;2;", color[0].to_s, ";", color[1].to_s, ";", color[2].to_s, "m"
+      put "48;2;#{color.join(";")}m"
     end
     @back = color
   end
 
   def print(*args)
+    return if x < 0 || x >= width || y < 0 || y >= height
+
     width = 0
     args.each do |a|
       width += a.size
     end
     @io.print(*args)
     @x += width
+  end
+
+  private
+
+  def put(sequence)
+    @io.print "#{CSI}#{sequence}"
+    self
   end
 end
